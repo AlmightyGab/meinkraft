01c69272e0c9e00c4a2cce9da76968c251edf08b
JFT70 <94490112+JFT70@users.noreply.github.com>
2025-07-27 19:18:42 -0400

    Basic ftxui document

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
-#include <iostream>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
 
+int main() {
+  using namespace ftxui;
 
+  // Create a simple document with three text elements.
+  Element document = hbox({
+    text("left")   | border,
+    text("middle") | border | flex,
+    text("right")  | border,
+  });
 
-int main(int argc, char* argv[])
-{
-  std::cout << "This is minecraft" << std::endl;
-  return 0;
+  // Create a screen with full width and height fitting the document.
+  auto screen = Screen::Create(
+    Dimension::Full(),       // Width
+    Dimension::Full() // Height
+  );
+
+  // Render the document onto the screen.
+  Render(screen, document);
+
+  // Print the screen to the console.
+  screen.Print();
 }
